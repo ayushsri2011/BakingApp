3a3acec8da3088fcc6f61709c97b7b25495c2032
Ayush <Ayushsri2011@gmail.com>
2018-08-18 23:38:50 +0530

    ok

--- app/src/androidTest/java/com/nightcrawler/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/nightcrawler/bakingapp/MainActivityTest.java
@@ -10,11 +10,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -23,17 +25,14 @@ public class MainActivityTest {
 
 
     @Test
-    public void ensureTextChangesWork() {
+    public void checkText() {
 
-        onView(withId(R.id.next_button)).perform(click());
         String c="1. Preheat the oven to 350\\u00b0F. Butter a 9\\\" deep dish pie pan.";
+
+        onView(withId(R.id.dish1)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView1)).atPosition(3).perform(click());
         onView(withId(R.id.details)).check(matches(withText(c)));
 
     }
 
-    @Test
-    public void checkText()
-    {
-        onView((withId(R.id.dish1))).perform(click());
-    }
 }
