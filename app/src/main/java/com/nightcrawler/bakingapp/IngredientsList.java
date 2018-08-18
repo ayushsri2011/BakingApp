105c7a9cb386eaaa54115c39b26d59aa1daa89f1
Ayush <Ayushsri2011@gmail.com>
2018-08-19 00:07:47 +0530

    ok

--- app/src/main/java/com/nightcrawler/bakingapp/IngredientsList.java
+++ app/src/main/java/com/nightcrawler/bakingapp/IngredientsList.java
@@ -2,7 +2,6 @@ package com.nightcrawler.bakingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
