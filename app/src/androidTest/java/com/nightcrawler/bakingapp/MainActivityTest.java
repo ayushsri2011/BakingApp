1553c98c973f6c0a29e78bdb4e909e898ffbe228
Ayush <Ayushsri2011@gmail.com>
2018-08-18 22:36:26 +0530

    ok

--- app/src/androidTest/java/com/nightcrawler/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/nightcrawler/bakingapp/MainActivityTest.java
@@ -12,17 +12,28 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule public ActivityTestRule<MainActivity> mainActivityActivityTestRule
                 =new ActivityTestRule<>(MainActivity.class);
 
+
     @Test
-    public void clickText()
+    public void ensureTextChangesWork() {
+
+        onView(withId(R.id.next_button)).perform(click());
+        String c="1. Preheat the oven to 350\\u00b0F. Butter a 9\\\" deep dish pie pan.";
+        onView(withId(R.id.details)).check(matches(withText(c)));
+
+    }
+
+    @Test
+    public void checkText()
     {
         onView((withId(R.id.dish1))).perform(click());
-
     }
 }
