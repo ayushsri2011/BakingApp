536230fbddd2344aaab904b4aa5d951ebbe96c12
Ayush <Ayushsri2011@gmail.com>
2018-08-19 23:11:25 +0530

    Final Commit before Submission.

--- app/src/main/java/com/nightcrawler/bakingapp/MainActivity.java
+++ app/src/main/java/com/nightcrawler/bakingapp/MainActivity.java
@@ -30,20 +30,16 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
     private Intent intent;
     private Bundle args;
-//    private RecyclerView recyclerView;
     private CustomAdapter adapter;
     private ArrayList<String> dishList;
     // --Commented out by Inspection (8/18/2018 11:41 PM):Toolbar toolbar;
-//    @BindView(R.id.dish1)
-//    TextView t1;
-//    @BindView(R.id.dish2)
-//    TextView t2;
-//    @BindView(R.id.dish3)
-//    TextView t3;
-//    @BindView(R.id.dish4)
-//    TextView t4;
+//    @BindView(R.id.dish1)    TextView t1;
+//    @BindView(R.id.dish2)    TextView t2;
+//    @BindView(R.id.dish3)    TextView t3;
+//    @BindView(R.id.dish4)    TextView t4;
     @BindView(R.id.bohe)
     RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         ButterKnife.bind(this);
 
         Resources res = getBaseContext().getResources();
-        String address= res.getString(R.string.address);
+        String address = res.getString(R.string.address);
         SharedPreferences sharedPreferences = getSharedPreferences("firstStart", 0);
         boolean firstStart = sharedPreferences.getBoolean("firstStart", true);
 
@@ -59,113 +55,42 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         if (Utils.checkConnectivity(this)) {
             Query q = new Query(MainActivity.this);
             q.execute(address);
-        }
-        else
+        } else
             Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
 
 
-//        intent = new Intent(MainActivity.this, DetailsActivity.class);
-
-//        t1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (Utils.checkConnectivity(getBaseContext())) {
-//                    args = new Bundle();
-//                    args.putInt("KEY", 1);
-//                    intent.putExtra("BUNDLE", args);
-//                    startActivity(intent);
-//                } else
-//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        t2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (Utils.checkConnectivity(getBaseContext())) {
-//                    Bundle args = new Bundle();
-//                    args.putInt("KEY", 2);
-//                    intent.putExtra("BUNDLE", args);
-//                    startActivity(intent);
-//                } else
-//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        t3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (Utils.checkConnectivity(getBaseContext())) {
-//                    Log.v("t3", "");
-//                    args = new Bundle();
-//                    args.putInt("KEY", 3);
-//                    intent.putExtra("BUNDLE", args);
-//                    startActivity(intent);
-//                } else
-//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        t4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (Utils.checkConnectivity(getBaseContext())) {
-//                    args = new Bundle();
-//                    args.putInt("KEY", 4);
-//                    intent.putExtra("BUNDLE", args);
-//                    startActivity(intent);
-//                } else
-//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-//        recyclerView= findViewById(R.id.bohe);
-
-
-
-
-
-
-
-//            String address = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-
-//            CollectionAppWidgetProvider.sendRefreshBroadcast(getBaseContext());
-
-
-
         try {
-            dishList=Utils.returnDishList(Utils.prefResponse(this));
+            dishList = Utils.returnDishList(Utils.prefResponse(this));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
 
 
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
-        adapter=new CustomAdapter(this);
+        adapter = new CustomAdapter(this);
         populateRecyclerViewValues(dishList);
     }
 
-    public void startRV()
-    {
+    public void startRV() {
         try {
-            dishList=Utils.returnDishList(Utils.prefResponse(this));
+            dishList = Utils.returnDishList(Utils.prefResponse(this));
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
 
-
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
-        adapter=new CustomAdapter(this);
+        adapter = new CustomAdapter(this);
         adapter.setStage(0);
         populateRecyclerViewValues(dishList);
     }
 
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        if(Utils.checkConnectivity(getBaseContext()))
-        getMenuInflater().inflate(R.menu.menu1, menu);
+        if (Utils.checkConnectivity(getBaseContext()))
+            getMenuInflater().inflate(R.menu.menu1, menu);
         return true;
     }
 
@@ -232,7 +157,9 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         adapter.setListContent(dishList);
         recyclerView.setAdapter(adapter);
     }
+}
 
+//COMMENTED CODE
 //    private void loadList(SharedPreferences sharedPreferences) {
 ////        movies_categories_key
 ////        String t=sharedPreferences.getString(getString(R.string.movies_categories_key));
@@ -260,4 +187,61 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 //            e.printStackTrace();
 //        }
 //    }
-}
+
+//------------------------------
+//        intent = new Intent(MainActivity.this, DetailsActivity.class);
+
+//        t1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (Utils.checkConnectivity(getBaseContext())) {
+//                    args = new Bundle();
+//                    args.putInt("KEY", 1);
+//                    intent.putExtra("BUNDLE", args);
+//                    startActivity(intent);
+//                } else
+//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        t2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (Utils.checkConnectivity(getBaseContext())) {
+//                    Bundle args = new Bundle();
+//                    args.putInt("KEY", 2);
+//                    intent.putExtra("BUNDLE", args);
+//                    startActivity(intent);
+//                } else
+//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        t3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (Utils.checkConnectivity(getBaseContext())) {
+//                    Log.v("t3", "");
+//                    args = new Bundle();
+//                    args.putInt("KEY", 3);
+//                    intent.putExtra("BUNDLE", args);
+//                    startActivity(intent);
+//                } else
+//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        t4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (Utils.checkConnectivity(getBaseContext())) {
+//                    args = new Bundle();
+//                    args.putInt("KEY", 4);
+//                    intent.putExtra("BUNDLE", args);
+//                    startActivity(intent);
+//                } else
+//                    Toast.makeText(MainActivity.this, "Ensure data connectivity", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        recyclerView= findViewById(R.id.bohe);
+//            String address = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+//            CollectionAppWidgetProvider.sendRefreshBroadcast(getBaseContext());
+
+
