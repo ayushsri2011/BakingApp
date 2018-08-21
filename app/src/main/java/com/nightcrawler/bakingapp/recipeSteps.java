80a8e754e9e7f66fe86ee756864940d90f8af098
Ayush <Ayushsri2011@gmail.com>
2018-08-22 00:47:13 +0530

    ok

--- app/src/main/java/com/nightcrawler/bakingapp/recipeSteps.java
+++ app/src/main/java/com/nightcrawler/bakingapp/recipeSteps.java
@@ -32,27 +32,4 @@ public class recipeSteps {
         this.description = description;
     }
 
-    public String getVideoURL() {
-        return videoURL;
-    }
-
-    public void setVideoURL(String videoURL) {
-        this.videoURL = videoURL;
-    }
-
-    public String getThumbnailURL() {
-        return thumbnailURL;
-    }
-
-    public void setThumbnailURL(String thumbnailURL) {
-        this.thumbnailURL = thumbnailURL;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
 }
