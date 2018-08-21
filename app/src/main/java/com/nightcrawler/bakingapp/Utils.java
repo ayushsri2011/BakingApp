fda73f94f34baf8d0f5068f8f429ab8262eb8183
Ayush <Ayushsri2011@gmail.com>
2018-08-22 00:47:37 +0530

    ok

--- app/src/main/java/com/nightcrawler/bakingapp/Utils.java
+++ app/src/main/java/com/nightcrawler/bakingapp/Utils.java
@@ -24,15 +24,6 @@ public class Utils {
 
         try {
 
-//            String address="https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-//            Query q = new Query();
-//            try {
-//                q.execute(address).get();
-//            } catch (InterruptedException | ExecutionException e) {
-//                e.printStackTrace();
-//            }
-
-//            final JSONArray jsonArray = new JSONArray(q.local_response);
             final JSONArray jsonArray = new JSONArray(response);
 
             int i = k - 1;
